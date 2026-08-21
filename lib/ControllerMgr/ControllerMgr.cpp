ac4d2c6e061c47fcc9ad7090fe2ff561abb804c5
Luckman Afandi <lukmanaffandi900@gmail.com>
2026-08-21 18:58:26 +0700

    Added Implementation for solving bezier curve

--- lib/ControllerMgr/ControllerMgr.cpp
+++ lib/ControllerMgr/ControllerMgr.cpp
@@ -1,23 +1,46 @@
-#include <Arduino.h>
-#include <cmath>
+#include "ControllerMgr.h"
 
-float getNormalizedAxis(
-    int rawValADC, int center,
-    int minVal, int maxVal, int dz)
-{
-    int delta = rawValADC - center;
 
-    if (std::abs(delta) <= dz)
-        return 0.0f;
 
-    if (delta > 0)
-    {
-        float maxPosRange = (maxVal - center) - dz;
-        return (float)(delta - dz) / maxPosRange;
+inline BezierSample ControllerMgr::getBezierSample(float t){
+    float u = 1.0f - t;
+    float u2 = u * u;
+    float t2 = t * t;
+    // Calculate x,y,dx value
+    float x = 3.0f * u2 * t * bezierControlX1 + 3.0f * u * t2 * bezierControlX2 + t * t2;
+    float y = 3.0f * u2 * t * bezierControlY1 + 3.0f * u * t2 * bezierControlY2 + t * t2;
+    float dx = 3.0f * (u2 * bezierControlX1 + 2.0f * u * t * (bezierControlX2 - bezierControlX1) + t2 * (1.0f - bezierControlX2));
+
+    return {x,y,dx};
+}
+
+float ControllerMgr::applyBezier(float target_x){
+    if(target_x <= 0.0f) return 0.0f;
+    if(target_x >= 1.0f) return 1.0f;
+
+    float lowerVal = 0.0f;
+    float upperVal = 1.0f;
+
+    float t = target_x;
+
+    for(int i = 0; i < 6; i++){
+        BezierSample sample = getBezierSample(t);
+        float err = sample.x - target_x;
+        if(fabs(err) < 1e-5f) return t;
+
+        float derivativeX = sample.dx;
+        if(fabs(derivativeX) < 1e-6f) t = 0.5f * (lowerVal + upperVal);
+        else {
+            float tNext = t - err / derivativeX;
+            if(tNext <= lowerVal || tNext >= upperVal) t = 0.5f * (lowerVal + upperVal);
+            else t = tNext;
+        }
+
+        // Shrink bracket
+        if (getBezierSample(t).x > target_x) upperVal = t;
+        else lowerVal = t;
+
     }
-    else
-    {
-        float maxNegRange = (center - minVal) - dz;
-        return (float)(delta + dz) / maxNegRange;
-    }
+
+    return t;
 }
--- lib/ControllerMgr/ControllerMgr.h
+++ lib/ControllerMgr/ControllerMgr.h
@@ -1,17 +1,31 @@
 #ifndef CONTROLLERMGR_H
 #define CONTROLLERMGR_H
 
-/**
- * Normalisasi nilai analog dengan Scaled Deadzone
- * @param rawValue   : Nilai ADC mentah (misal: 0 - 4095)
- * @param center     : Nilai titik tengah (misal: 2048)
- * @param minVal     : Nilai ADC minimum (0)
- * @param maxVal     : Nilai ADC maksimum (4095)
- * @param deadzone   : Nilai offset deadzone (misal: 150)
- * @return float     : Output ter-skala dari -1.0f (mundur/kiri) sampai 1.0f (maju/kanan)
- */
-float getNormalizedAxis(
-    int rawValADC, int center,
-    int minVal, int maxVal, int dz);
+#include <Arduino.h>
+#include <cmath>
+
+struct BezierSample
+{
+    float x;
+    float y;
+    float dx; // derivative dx/dt
+};
+
+class ControllerMgr {
+    private:
+        static const int16_t BASE_MOTOR_SPD = 100;
+        static const int16_t MAX_MOTOR_SPD = 255;
+        static const uint8_t MIN_DZ = 15;
+        static const uint8_t BOOST_DZ = 25;
+        static const uint8_t MAX_DZ = 40;
+
+        float bezierControlX1, bezierControlX2, bezierControlY1, bezierControlY2; 
+        
+        float applyBezier(float target_x);
+        inline BezierSample getBezierSample(float t);
+    public:
+        
+};
+
 
 #endif // CONTROLLERMGR_H
