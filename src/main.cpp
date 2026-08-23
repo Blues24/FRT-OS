cde5db569794b94dd51d67b43e3fccea2c7da623
diding <didinghidayat341@gmail.com>
2026-08-23 23:41:22 +0700

    revisi CodeRabbit 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,37 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "ServoMgr.h"
+#include "WSMgr.h"
+<<<<<<< HEAD
+=======
 
-// put function declarations here:
-int myFunction(int, int);
+// Password WPA2 untuk hotspot (minimal 8 karakter)
+static const char* AP_SSID = "FRT-OS";
+static const char* AP_PASS = "frt12345";
+>>>>>>> test
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+
+    // Inisialisasi servo
+    servoInit();
+
+<<<<<<< HEAD
+    // Hotspot WiFi ESP32
+    WiFi.softAP("RTOS-setup", "");
+    Serial.print("Web Controller siap di: http://");
+    Serial.println(WiFi.softAPIP());
+=======
+    // Hotspot WiFi ESP32 dengan password WPA2
+    WiFi.softAP(AP_SSID, AP_PASS);
+    Serial.print("Web Controller siap di: http://");
+    Serial.println(WiFi.softAPIP());
+    Serial.print("Password WiFi: ");
+    Serial.println(AP_PASS);
+>>>>>>> test
+
+    // Jalankan Web Server
+    wsMgrBegin();
 }
 
 void loop() {
